134b69fa0cd4e875940204521d3e02983819b98f
gwyrwch <gwyrwch@gmail.com>
2019-12-05 00:37:37 +0300

    plotting tours added to research.ipynb

---
+++ algo/GeneticAlgorithm.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rita Gagarina on 12/4/19.
+//
+
+#include "GeneticAlgorithm.h"
---
+++ algo/GeneticAlgorithm.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algo/IOptimizer.h>
+#include <lib/Timer.h>
+
+namespace NAlgo {
+    class GeneticAlgorithm : public IOptimizer {
+    public:
+        explicit GeneticAlgorithm(int version, OptimizerConfig config)
+                : IOptimizer(version, std::move(config))
+        {}
+        Tour optimize(const Test& test, Tour baseline) override {
+
+        }
+
+        std::string optimizer_name() const override  {
+            return "LocalSearch";
+        }
+
+    private:
+
+    };
+}
+
